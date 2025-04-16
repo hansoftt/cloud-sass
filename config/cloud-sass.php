6a2974125b15a7369da752c6b196d39f40175f67
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2025-04-16 12:40:01 +0530

    Merge config

--- config/cloud-sass.php
+++ config/cloud-sass.php
@@ -2,5 +2,19 @@
 
 // config for Hansoft/CloudSass
 return [
-
+    'connections' => [
+        'client' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', '127.0.0.1'),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => '',
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+            'engine'    => 'InnoDB',
+        ],
+    ],
 ];
