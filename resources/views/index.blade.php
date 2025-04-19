ec8ad40b04b5fb030e382d2844f0cd22a375a29a
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2025-04-19 17:55:32 +0530

    Added Content

--- resources/views/index.blade.php
+++
@@ -1,8 +0,0 @@
-@component(config('cloud-sass::layouts-app'))
-    @section('title', 'Cloud SASS')
-    <div class="container">
-        <h1>Welcome to Cloud SASS</h1>
-        {{ dump(config('cloud-sass.migrations_location')) }}
-        <p>Your one-stop solution for cloud-based SASS applications.</p>
-    </div>
-@endcomponent
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,3 +1,26 @@
 @extends('cloud-sass::layouts.app')
 
 @section('title', 'Projects')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Projects') }}</div>
+
+                    <div class="card-body">
+                        <div>
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
